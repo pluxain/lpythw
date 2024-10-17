02bd209cf51236a2d5f796a8719aa2b231359051
Pascal Luxain <pascal.luxain@gmail.com>
2024-10-17 21:26:04 +0200

    ex3: done

---
+++ ex003.py
@@ -0,0 +1,55 @@
+# print a sentence
+print("I will now count my chickens:")
+
+# print the result of one operation
+print("Hens", 25 + 30 / 6)
+
+# print the result of another operation
+print("Roosters", 100 - 25 * 3 % 7)
+
+print("Now I will count the eggs:")
+
+# print the final result
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+print("Is it true that 3 + 2 < 5 - 7?")
+# print the result of the comparison after doing operations
+print(3 + 2 < 5 - 7)
+
+
+print("What is 3 + 2?", 3 + 2)
+print("What is 5 - 7?", 5 - 7)
+
+print("Oh, that's why it is False.")
+
+print("How about some more.")
+print("Is is greater?", 5 > -2)
+print("Is it greater or equal?", 5 >= -2)
+print("Is it less or equal?", 5 <= -2)
+
+# This is not printed when running the file with python3 interpreter
+1 + 2
+
+
+print("Using floats")
+print("I will now count my chickens:")
+
+print("Hens", 25.0 + 30 / 6)
+print("Roosters", 100.0 - 25 * 3 % 7)
+
+print("Now I will count the eggs:")
+
+print(3.0 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+print("Is it true that 3 + 2 < 5 - 7?")
+print(3.0 + 2 < 5 - 7)
+
+print("What is 3 + 2?", 3.0 + 2)
+print("What is 5 - 7?", 5.0 - 7)
+
+print("Oh, that's why it is False.")
+
+print("How about some more.")
+print("Is is greater?", 5.0 > -2)
+print("Is it greater or equal?", 5.0 >= -2)
+print("Is it less or equal?", 5.0 <= -2)
