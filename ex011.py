98e85b083d92b407a55a009162fdf8378e51af3c
Pascal Luxain <pascal.luxain@gmail.com>
2024-10-20 19:24:18 +0200

    ex11: do study drills

--- ex011.py
+++ ex011.py
@@ -1,8 +1,5 @@
-print("How old are you ?", end=' ')
-age = input()
-print("How tall are you ?", end=' ')
-height = input()
-print("How much do you weigh?", end=' ')
-weight = input()
+age = input("How old are you ?")
+height = input("How tall are you ?")
+weight = input("How much do you weigh?")
 
 print(f"So, you're {age} old, {height} tall and {weight} heavy.")
