ba2de3062c36853b171ed14c2134fcc01ffe31a3
Pascal Luxain <pascal.luxain@gmail.com>
2024-10-22 09:32:17 +0200

    ex15: do study drills

--- ex015.py
+++ ex015.py
@@ -1,14 +1,22 @@
+# import argv from sys package
 from sys import argv
 
+# unpack argv into variables the first one is the name of the executed script
 script, filename = argv
 
+# open the file according to the argument passed
 txt = open(filename)
 
+# print the filename and content
 print(f"Here's your file {filename}")
 print(txt.read())
+txt.close()
 
+# ask fo another filename
 print("Type the filename again:")
 file_again = input("> ")
 
+# open and read the other filename
 txt_again = open(file_again)
 print(txt_again.read())
+txt_again.close()
