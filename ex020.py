5b50a78b084b94f33972f533ba4c2461af72d9e6
Pascal Luxain <pascal.luxain@gmail.com>
2024-11-06 14:50:39 +0100

    ex20: do study drills

--- ex020.py
+++ ex020.py
@@ -1,16 +1,21 @@
 from sys import argv
 
-input_file = "ex020_test.txt"
+# input_file = "ex020_test.txt"
+script, input_file = argv
+
+# print a file content
 
 
 def print_all(f):
     print(f.read())
 
 
+# rewind a file
 def rewind(f):
     f.seek(0)
 
 
+# print the next line of the file
 def print_a_line(line_count, f):
     print(line_count, f.readline())
 
@@ -25,11 +30,14 @@ rewind(current_file)
 
 print("Let's print three lines:")
 current_line = 1
+# current_line is 1
 print_a_line(current_line, current_file)
 
 
-current_line = current_line + 1
+current_line += 1
+# current_line is 2
 print_a_line(current_line, current_file)
 
-current_line = current_line + 1
+current_line += 1
+# current_line is 3
 print_a_line(current_line, current_file)
