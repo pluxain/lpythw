f458243d5e61fd3b14aa59f40e5fc843809bf981
Pascal Luxain <pascal.luxain@gmail.com>
2024-11-13 16:40:33 +0100

    ex25: do study drills

--- ex025.py
+++ ex025.py
@@ -35,3 +35,35 @@ corvette = {
 
 print("My", corvette["color"], "can go")
 corvette["run"]()
+
+
+def create_car(color, name, sound, speed):
+    def go():
+        print(f"{sound}!!")
+        return sound
+
+    return {
+        "color": color,
+        "name": name,
+        "go": go,
+        "speed": speed
+    }
+
+
+corvette = create_car("red", "corvette", "BRAAAAAAAM", 120)
+
+print(f"Hey! I am {corvette["name"]}, I am {
+      corvette["color"]} and can drive up to {corvette["speed"]}, {corvette["go"]()}!!")
+
+audi = create_car("red", "audi", "BROOOOOOM", 160)
+
+print(f"Hey! I am {audi["name"]}, I am {
+      audi["color"]} and can drive up to {audi["speed"]}, {audi["go"]()}!!")
+
+corvette["color"] = "black"
+
+print(f"Hey! I am {corvette["name"]}, I am {
+      corvette["color"]} and can drive up to {corvette["speed"]}, {corvette["go"]()}!!")
+
+print(f"Hey! I am {audi["name"]}, I am {
+      audi["color"]} and can drive up to {audi["speed"]}, {audi["go"]()}!!")
