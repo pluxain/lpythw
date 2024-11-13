d5134c9ddc9c2da1f471667ec1c6b9a4a6ba67a9
Pascal Luxain <pascal.luxain@gmail.com>
2024-11-13 16:50:33 +0100

    ex26: do step 2

--- ex026_code.py
+++ ex026_code.py
@@ -1,6 +1,11 @@
 import ex026
 import ex026  # that seems to do nothing
+from pprint import pprint
 
 print("name", ex026.name)
 print("height", ex026.height)
 print("age", ex026.age)
+
+pprint(ex026.__dict__)
+print("height is", ex026.height)
+print("height is", ex026.__dict__["height"])
