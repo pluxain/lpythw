ccec25750d4d32fbdc48e3197fa75d4f1f83a093
Pascal Luxain <pascal.luxain@gmail.com>
2024-11-14 09:09:16 +0100

    ex27: do rule 3

--- ex027.py
+++ ex027.py
@@ -7,3 +7,27 @@ z = x + y
 ''')
 
 dis("while True: x = 10")
+
+dis('''
+x = 1
+if x > 0:
+    y = 10
+''')
+
+dis('''
+x = -1
+if x > 0:
+    y = 10
+''')
+
+x = 10
+while x > 0:
+    x -= 1
+    print(f"x is {x}")
+
+dis('''
+x = 10
+while x > 0:
+    x -=1
+    print(f"x is {x}")
+''')
