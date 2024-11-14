90c3a7de15dc208802d4a42b6e64c78779a320c3
Pascal Luxain <pascal.luxain@gmail.com>
2024-11-14 09:19:33 +0100

    ex27: do rule 5 (no code for rule 4)

--- ex027.py
+++ ex027.py
@@ -1,19 +1,35 @@
 from dis import dis
 
+print('''
+x = 10
+y = 20
+z = x + y
+''')
 dis('''
 x = 10
 y = 20
 z = x + y
 ''')
 
+print("while True: x = 10")
 dis("while True: x = 10")
 
+print('''
+x = 1
+if x > 0:
+    y = 10
+''')
 dis('''
 x = 1
 if x > 0:
     y = 10
 ''')
 
+print('''
+x = -1
+if x > 0:
+    y = 10
+''')
 dis('''
 x = -1
 if x > 0:
@@ -25,9 +41,18 @@ while x > 0:
     x -= 1
     print(f"x is {x}")
 
+print('''
+x = 10
+while x > 0:
+    x -=1
+    print(f"x is {x}")
+''')
 dis('''
 x = 10
 while x > 0:
     x -=1
     print(f"x is {x}")
 ''')
+
+print("input('Yes? ')")
+dis("input('Yes? ')")
